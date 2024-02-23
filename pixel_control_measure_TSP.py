3fee042b1dcf1c8af31c61cae39b88ff81e1f54b
js615630 <105065575+js615630@users.noreply.github.com>
2024-02-22 22:24:26 -0400

    1. Fixed indentation

--- pixel_control_measure_TSP.py
+++ pixel_control_measure_TSP.py
@@ -41,7 +41,7 @@ class PixelControlSystem:
         # Initialize serial connection
         self.ser = serial.Serial('COM7', 9600)
 
-        # Define pixel commands
+        # Define pixel commands #
         self.pixel_commands = {
             1: ('o', 'r'),
             2: ('o', 'g'),
@@ -92,89 +92,76 @@ class PixelControlSystem:
         rm = pyvisa.ResourceManager()
         try:
             smu = rm.open_resource('USB0::0x05E6::0x2450::04387860::INSTR')
-        tsp_script = f"""-- Define the number of points in the sweep.
-        num = {self.step_count}
+            tsp_script = f"""
+                    -- Define the number of points in the sweep.
+                    num = {self.step_count}
 
-        -- Reset the instrument and clear the buffer.
-        reset()
+                    -- Reset the instrument and clear the buffer.
+                    reset()
 
-        -- Set the source and measure functions.
-        smu.measure.func = smu.FUNC_DC_CURRENT
-        smu.source.func = smu.FUNC_DC_VOLTAGE
+                    -- Set the source and measure functions.
+                    smu.measure.func = smu.FUNC_DC_CURRENT
+                    smu.source.func = smu.FUNC_DC_VOLTAGE
 
-        -- Measurement settings.
-        smu.terminals = smu.TERMINALS_FRONT
-        smu.measure.sense = smu.SENSE_4WIRE
-        smu.measure.autorange = smu.ON
-        smu.measure.nplc = 1
+                    -- Measurement settings.
+                    smu.terminals = smu.TERMINALS_FRONT
+                    smu.measure.sense = smu.SENSE_4WIRE
+                    smu.measure.autorange = smu.ON
+                    smu.measure.nplc = 1
 
-        -- Source settings.
-        smu.source.highc = smu.OFF
-        smu.source.range = 2
-        smu.source.readback = smu.ON
-        smu.source.ilimit.level = 1
-        smu.source.sweeplinear("SolarCell", {self.start_voltage}, {self.stop_voltage}, num, {self.step_delay})
+                    -- Source settings.
+                    smu.source.highc = smu.OFF
+                    smu.source.range = 2
+                    smu.source.readback = smu.ON
+                    smu.source.ilimit.level = 1
+                    smu.source.sweeplinear("SolarCell", {self.start_voltage}, {self.stop_voltage}, num, {self.step_delay})
 
-        -- Start the trigger model and wait for it to complete.
-        trigger.model.initiate()
-        waitcomplete()
+                    -- Start the trigger model and wait for it to complete.
+                    trigger.model.initiate()
+                    waitcomplete()
 
-        -- Define
-        initial
-        values.
-        voltage = defbuffer1.sourcevalues
-        current = defbuffer1
-        isc = current[1]
-        mincurr = current[1]
-        imax = current[1]
-        voc = voltage[1]
-        vmax = voltage[1]
-        pmax = voltage[1] * current[1]
+                    -- Define initial values.
+                    voltage = defbuffer1.sourcevalues
+                    current = defbuffer1
+                    isc = current[1]
+                    mincurr = current[1]
+                    imax = current[1]
+                    voc = voltage[1]
+                    vmax = voltage[1]
+                    pmax = voltage[1] * current[1]
 
-        -- Calculate
-        values.
-        for i = 1, num do
-        print(voltage[i], current[i], voltage[i] * current[i])
-        if (voltage[i] * current[i] < pmax) then
-        pmax = voltage[i] * current[i]
-        imax = current[i]
-        vmax = voltage[i]
+                    -- Calculate values.
+                    for i = 1, num do
+                        print(voltage[i], current[i], voltage[i] * current[i])
+                        if (voltage[i] * current[i] < pmax) then
+                            pmax = voltage[i] * current[i]
+                            imax = current[i]
+                            vmax = voltage[i]
+                        end
+                        if math.abs(current[i]) < math.abs(mincurr) then
+                            voc = voltage[i]
+                        end
+                    end
 
-    end
-    if math.abs(current[i]) < math.abs(mincurr) then
-    voc = voltage[i]
+                    pmax = math.abs(pmax)
+                    imax = math.abs(imax)
 
+                    -- Print calculated maximum power, current, and voltage
+                    print("Pmax = ", pmax, ", Imax = ", imax, ", Vmax = ", vmax, ", Isc = ", isc, ", Voc = ", voc)
 
-end
-end
+                    -- Display values on the front panel.
+                    display.changescreen(display.SCREEN_USER_SWIPE)
+                    display.settext(display.TEXT1, string.format("Pmax = %.4fW", pmax))
+                    display.settext(display.TEXT2, string.format("Isc = %.4fA, Voc = %.2fV", isc, voc))
+                    """
 
-pmax = math.abs(pmax)
-imax = math.abs(imax)
+            # Send the TSP script to the instrument
+            smu.write(tsp_script)
 
--- Print
-calculated
-maximum
-power, current, and voltage
-print("Pmax = ", pmax, ", Imax = ", imax, ", Vmax = ", vmax, ", Isc = ", isc, ", Voc = ", voc)
+            # Read the response (if applicable)
+            response = smu.read()
+            print(response)
 
--- Display
-values
-on
-the
-front
-panel.
-display.changescreen(display.SCREEN_USER_SWIPE)
-display.settext(display.TEXT1, string.format("Pmax = %.4fW", pmax))
-display.settext(display.TEXT2, string.format("Isc = %.4fA, Voc = %.2fV", isc, voc))
-"""
-
-# Send the TSP script to the instrument
-smu.write(tsp_script)
-
-# Read the response (if applicable)
-response = smu.read()
-print(response)
-
-finally:
-        # Close the connection
-        smu.close()
+        finally:
+            # Close the connection
+            smu.close()
